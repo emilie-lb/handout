c6470ebffeb36d5e4f3f3b4b056553d9503e96a2
emilie-lb <emilielb75@gmail.com>
2021-05-06 10:16:25 +0200

    nouveau découpage de certaines fonctions dans parse_log.py et ajout de quelques tests dans test_parse_log.py

--- parse_log.py
+++ parse_log.py
@@ -4,28 +4,30 @@ import argparse
 
 logging.basicConfig(level=logging.DEBUG)
 
-
-
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument(dest='path', type=str, help='chemin vers fichier .log')
 args = parser.parse_args()
 # print(args)
 
+
 def open_log_file(path):
-    logging.info("entrée dans fonction open_log_file")
+    logging.debug(f"fonction open_log_file, fichier à ouvrir {path}")
     liste_logs = []
     with open(path, "r") as log_file:
+        logging.debug(f"entrée dans fichier{path}")
         for line in log_file.readlines():
             if line != "\n":
                 liste_logs += [line.strip()]
+    logging.debug(f"ligne 1 de la liste de logs => {liste_logs[0]}")
     return liste_logs
 
 
 def delai_min(line):
     date_time1 = datetime.datetime.strptime(line[0:5], '%H:%M')
     date_time2 = datetime.datetime.strptime(line[6:11], '%H:%M')
-    timedelta= date_time2-date_time1
+    timedelta = date_time2-date_time1
     return int(timedelta.total_seconds() / 60)
+
 
 def create_delai_list(log):
     delai = delai_min(log)
@@ -34,45 +36,51 @@ def create_delai_list(log):
 
 
 def temps_total(dico):
-    total=0
+    total = 0
     for min in dico.values():
         total += min
     return total
 
 
 def write_file(liste):
+    logging.debug("entrée dans fonction write_file")
     f = open("expected_output.txt", "w")
     for ligne in liste:
         f.write(ligne + "\n")
 
+
 def liste_lignes_export(dico, total):
-    liste_lignes=[]
+    logging.debug("entrée dans fonction liste_lignes_export")
+    liste_lignes = []
     for key in sorted(dico.keys()):
         percent = dico[key]*100//total
-        str_ok=f"{key:<20}{dico[key]:>9} minutes{percent:>5}%"
-        # print(str_ok)
-        # print(len(str_ok))
+        str_ok = f"{key:<20}{dico[key]:>9} minutes{percent:>5}%"
         liste_lignes.append(str_ok)
+    logging.debug(f"ligne 1 de liste_lignes =>\n{liste_lignes[0]}")
+    logging.debug(f"longueur de la 1ere ligne => {len(liste_lignes[0])}")
     return liste_lignes
 
-def main(path):
-    liste_logs = open_log_file(path)
-    liste_delai=[]
+
+def create_dico_delai(liste):
     dico_delai = {}
-    for log in liste_logs:
-        liste_delai.append(create_delai_list(log))
-    for ligne in liste_delai:
+    for ligne in liste:
         if ligne[1] in dico_delai:
             dico_delai[ligne[1]] += ligne[0]
         else:
-            dico_delai[ligne[1]]=ligne[0]
+            dico_delai[ligne[1]] = ligne[0]
+    return dico_delai
+
+
+def main(path):
+    liste_logs = open_log_file(path)
+    liste_delai = []
+    for log in liste_logs:
+        liste_delai.append(create_delai_list(log))
+    dico_delai = create_dico_delai(liste_delai)
     total = temps_total(dico_delai)
     liste_lignes = liste_lignes_export(dico_delai, total)
-
-    
     write_file(liste_lignes)
 
-    
 
 if __name__ == '__main__':
     main(args.path)
--- test_parse_log.py
+++ test_parse_log.py
@@ -1,21 +1,43 @@
 import os
 import unittest
 
-TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'test_planning.log')
+from parse_log import *
 
-class BasicTest(unittest.TestCase):
+TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'planning.log')
+
+
+class TestBasic(unittest.TestCase):
     """Basic test cases."""
 
     # à voir pour tester ouverture du fichier.log (à adapter)
     def setUp(self):
        self.testdata = open(TESTDATA_FILENAME).read()
-
-    def test_intermediaire(self):
-        self.assertEqual(intermediaire("12:30-12:45 Dictionaries"), "Dictionaries               15 minutes  100%"  )
+       self.dict_delai = {"test1":55, "test2":15, "test3":20}
 
 
-    def test_final(self):
-        self.assertEqual(final(TESTDATA_FILENAME),  )
+    # def open_log_file(path):
+
+
+    def test_delai_min(self):
+        self.assertEqual(delai_min("09:20-11:00 Introduction"), 100)
+        self.assertEqual(delai_min("12:30-12:45 Dictionaries"), 15)
+
+    def test_create_delai_list(self):
+        self.assertEqual(create_delai_list("09:20-11:00 Introduction"), [100, "Introduction"])
+        self.assertEqual(create_delai_list("11:35-12:30 Numbers and strings"), [55, "Numbers and strings"])
+        self.assertIsInstance(create_delai_list("11:35-12:30 Numbers and strings"), list)
+        self.assertIsNotNone(create_delai_list("11:35-12:30 Numbers and strings"))
+
+    def test_temps_total(self):
+        self.assertEqual(temps_total(self.dict_delai), 90)
+        self.assertIsInstance(temps_total(self.dict_delai), int)
+
+    # def test_write_file(self):
+
+    # def test_liste_lignes_export(self):
+    
+    # def test_create_dico_delai(self):
+        # self.assertIsInstance(self.liste, dict)
 
 
 
