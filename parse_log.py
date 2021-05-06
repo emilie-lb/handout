1f878922b394ff1f095a87c17598230512c3f301
emilie-lb <emilielb75@gmail.com>
2021-05-06 22:31:44 +0200

    ajout des docstring, ajout de tests, rangement du code

--- parse_log.py
+++ parse_log.py
@@ -7,11 +7,15 @@ logging.basicConfig(level=logging.DEBUG)
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument(dest='path', type=str, help='chemin vers fichier .log')
 args = parser.parse_args()
-# print(args)
 
 
 def open_log_file(path):
-    logging.debug(f"fonction open_log_file, fichier à ouvrir {path}")
+    """ Cette fonction ouvre le fichier .log et enregistre les lignes
+    dans une liste
+    Parametre : chemin vers le fichier .log
+    Return : liste contenant toutes les lignes du fichier .log en format
+    """
+    logging.info(f"fonction open_log_file, fichier à ouvrir {path}")
     liste_logs = []
     with open(path, "r") as log_file:
         logging.debug(f"entrée dans fichier{path}")
@@ -22,34 +26,76 @@ def open_log_file(path):
     return liste_logs
 
 
-def delai_min(line):
-    date_time1 = datetime.datetime.strptime(line[0:5], '%H:%M')
-    date_time2 = datetime.datetime.strptime(line[6:11], '%H:%M')
+def delai_min(log):
+    """ Cette fonction calcule le temps (ou délai) d'une activité donnée
+    Parametre : une string contenant une ligne du fichier .log
+    (ex: "09:20-11:00 Introduction")
+    Return : temps de l'activité en minute (type integer)
+    (ex: 100)
+    """
+    date_time1 = datetime.datetime.strptime(log[0:5], '%H:%M')
+    date_time2 = datetime.datetime.strptime(log[6:11], '%H:%M')
     timedelta = date_time2-date_time1
     return int(timedelta.total_seconds() / 60)
 
 
 def create_delai_list(log):
+    """ Cette fonction extrait d'une line de log l'activité et calcule le
+    délai de cette activité en min (en appelant la fonction delai_min(log))
+    Parametre : une string contenant une ligne du fichier .log
+    (ex: "09:20-11:00 Introduction")
+    Return : une liste
+    (ex: ["Exercises", 100])
+    """
     delai = delai_min(log)
     activity = log.strip().split(" ", 1)[1]
     return [delai, activity]
 
 
+def create_dico_delai(liste):
+    """ Cette fonction crée un dictionnaire contenant les noms des
+    activités (clés) et les temps cumulés pour chaque activité (valeurs)
+    Parametre : une liste de de listes contenant 2 éléments,
+    l'activité (string) et le délai en minutes (integer).
+    (ex: [["Introduction", 40], ["Exercises", 100], ["Exercises", 105])
+    Return : un dictionnaire contenant les activités (clés) et le temps cumulé
+    des activités (valeurs)
+    """
+    dico_delai = {}
+    for ligne in liste:
+        if ligne[1] in dico_delai:
+            dico_delai[ligne[1]] += ligne[0]
+        else:
+            dico_delai[ligne[1]] = ligne[0]
+    return dico_delai
+
+
 def temps_total(dico):
+    """ Cette fonction calcule le temps total des activités en minutes
+    Parametre : un dico contenant les activités (clés) et le temps cumulé
+    des activités (valeurs)
+    Return : integer représentant le temps total de toutes les activités
+    en minutes.
+    """
     total = 0
-    for min in dico.values():
-        total += min
+    for minutes in dico.values():
+        total += minutes
+    logging.debug(f"temps total de toutes les activités {total}")
     return total
 
 
-def write_file(liste):
-    logging.debug("entrée dans fonction write_file")
-    f = open("expected_output.txt", "w")
-    for ligne in liste:
-        f.write(ligne + "\n")
-
-
 def liste_lignes_export(dico, total):
+    """ Cette fonction met en forme les lignes de texte que le fichier
+    de sortie contiendra
+    Parametre :
+    - un dico contenant les activités (clés) et le temps cumulé
+    des activités (valeurs)
+    - un integer (le temps total cumulé de toutes les activités)
+    Return : une liste contenant les lignes de texte que le fichier
+    de sortie contiendra (avec une mise respectant les contraintes
+    d'alignement et de longueur de texte)
+    (ex: "Break                      65 minutes    6%")
+    """
     logging.debug("entrée dans fonction liste_lignes_export")
     liste_lignes = []
     for key in sorted(dico.keys()):
@@ -61,14 +107,16 @@ def liste_lignes_export(dico, total):
     return liste_lignes
 
 
-def create_dico_delai(liste):
-    dico_delai = {}
+def write_file(liste):
+    """ Cette fonction crée un fichier puis elle écrit les lignes sous
+    leur forme finale dans ce fichier.
+    Parametre : une liste contenant les lignes à ajouter au fichier
+    de sortie
+    """
+    logging.debug("entrée dans fonction write_file")
+    f = open("output.txt", "w")
     for ligne in liste:
-        if ligne[1] in dico_delai:
-            dico_delai[ligne[1]] += ligne[0]
-        else:
-            dico_delai[ligne[1]] = ligne[0]
-    return dico_delai
+        f.write(ligne + "\n")
 
 
 def main(path):
@@ -84,6 +132,3 @@ def main(path):
 
 if __name__ == '__main__':
     main(args.path)
-
-# print(main("planning.log"))
-# main("planning.log")
--- test_parse_log.py
+++ test_parse_log.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-from parse_log import *
+from parse_log import delai_min, create_delai_list, temps_total, create_dico_delai
 
 TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'planning.log')
 
@@ -11,12 +11,10 @@ class TestBasic(unittest.TestCase):
 
     # à voir pour tester ouverture du fichier.log (à adapter)
     def setUp(self):
-       self.testdata = open(TESTDATA_FILENAME).read()
-       self.dict_delai = {"test1":55, "test2":15, "test3":20}
-
-
-    # def open_log_file(path):
-
+        self.testdata = open(TESTDATA_FILENAME).read()
+        self.dict_delai = {"test1": 55, "test2": 15, "test3": 20}
+        self.liste_delai = [[40, "test1"], [100, "test2"], [105, "test2"]]
+        self.total = 245
 
     def test_delai_min(self):
         self.assertEqual(delai_min("09:20-11:00 Introduction"), 100)
@@ -32,15 +30,14 @@ class TestBasic(unittest.TestCase):
         self.assertEqual(temps_total(self.dict_delai), 90)
         self.assertIsInstance(temps_total(self.dict_delai), int)
 
-    # def test_write_file(self):
+    def test_create_dico_delai(self):
+        self.assertEqual(create_dico_delai(self.liste_delai), {"test1": 40, "test2": 205})
+        self.assertIsInstance(create_dico_delai(self.liste_delai), dict)
 
+    # def open_log_file(path):
     # def test_liste_lignes_export(self):
-    
-    # def test_create_dico_delai(self):
-        # self.assertIsInstance(self.liste, dict)
-
+    # def test_write_file(self):
 
 
 if __name__ == '__main__':
     unittest.main()
-
